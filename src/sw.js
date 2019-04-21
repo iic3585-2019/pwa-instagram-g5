8efa14d672ae47d2328011ee9acaad1a77caf333
Francisco <fco.olivaress@gmail.com>
2019-04-20 22:30:50 -0400

    User workbox to catch

--- src/sw.js
+++ src/sw.js
@@ -1,14 +1,8 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.2.0/workbox-sw.js')
+const { strategies } = workbox
 // Rutas de lo que se está cacheando
-/*const staticAssets = [
-    './',
-    './style.css',
-    './index.js',
-    './bulma.min.css'
-]*/
-
 const staticAssets = [
-    './',
-    './app.js'
+    './'
 ]
 
 self.addEventListener('install', async event => {
@@ -20,15 +14,45 @@ self.addEventListener('fetch', event => {
     const req = event.request
     const url = new URL(req.url)
 
+    const cacheFirst = new strategies.CacheFirst();
+    const networkFirst = new workbox.strategies.NetworkFirst()
+
     if (url.origin === location.origin) {
-        event.respondWith(cacheFirst(req))
+        event.respondWith(cacheFirst.makeRequest({ request: event.request }))
     } else {
-        // event.respondWith(networkFirst(req))
+        event.respondWith(networkFirst.makeRequest({ request: event.request }))
     }
 
 })
+
+/*
 
 async function cacheFirst(req) {
     const cachedResponse = await caches.match(req)
     return cachedResponse || fetch(req)
 }
+
+async function networkFirst(req) {
+    const cache = await caches.open('app-dynamic')
+
+    try {
+        const res = await fetch(req)
+        cache.put(req, res.clone())
+        return res
+    } catch (error) {
+        return await cache.match(req)
+    }
+
+}*/
+
+/*
+workbox.routing.registerRoute(
+    new RegExp('.*\.js'),
+    new workbox.strategies.NetworkFirst()
+)
+
+//Index.html
+workbox.routing.registerRoute(
+    new RegExp(''),
+    new workbox.strategies.NetworkFirst()
+);*/
